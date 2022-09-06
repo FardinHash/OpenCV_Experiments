1241c6da67f49821fd139c6274b0bf108bdf3083
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-06 22:48:10 +0600

    Update read-image.py

--- read-image.py
+++ read-image.py
@@ -1,7 +1,7 @@
-import cv2
+import cv2  #import library
 
-image= cv2.imread('images/images1.jpg')
+image= cv2.imread('images/images1.jpg')  #load the image to read
 
-cv2.imshow('Hello AI', image)
+cv2.imshow('Hello AI', image)  #show the image
 
 cv2.waitKey(0)
