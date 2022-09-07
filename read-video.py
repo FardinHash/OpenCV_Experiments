8568c0a1ab031a148bfc4ee349901aa065864dff
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-08 02:14:01 +0600

    Update read-video.py

--- read-video.py
+++ read-video.py
@@ -1,15 +1,16 @@
-import cv2
+import cv2  #import library
 
+#set height and width
 h= 640
 w= 480
 
-cap= cv2.VideoCapture('sample video.mp4')
+cap= cv2.VideoCapture('sample video.mp4')  #load the video
 
 while True:
      success, image= cap.read()
-     image = cv2.resize(image, (h, w))
+     image = cv2.resize(image, (h, w))  #resize with height and width
 
-     cv2.imshow('Video', image)
+     cv2.imshow('Video', image)  #show the video
      if cv2.waitKey(1)&0xFF== ord('q'):
 
          break
