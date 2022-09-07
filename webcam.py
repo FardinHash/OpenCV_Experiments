a5c817b944648e24e2b2b955b1bccbbb8b8935fc
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-09-08 02:17:02 +0600

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,18 +1,21 @@
-import cv2
+import cv2  #import library 
 
+#set height and width
 h= 640
 w= 480
 
-cap= cv2.VideoCapture(0)
+cap= cv2.VideoCapture(0)  #access the webcam
+
 cap.set(3, h)
 cap.set(4, w)
 cap.set(10, 150)
 
 while True:
      success, image= cap.read()
-     image = cv2.resize(image, (h, w))
+     image = cv2.resize(image, (h, w))  #resize with height and width
 
-     cv2.imshow('Video', image)
-     if cv2.waitKey(1)&0xFF== ord('q'):
+     cv2.imshow('Video', image)  #show the output
+     
+     if cv2.waitKey(1)&0xFF== ord('q'):  #escape!
 
          break
