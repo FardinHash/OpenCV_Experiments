562c120a5fe3de567d14a3affd8d5d6438ba0fc9
Fardin <60337534+FardinHash@users.noreply.github.com>
2022-10-14 01:53:38 +0600

    Update shapes and text.py

--- shapes and text.py
+++ shapes and text.py
@@ -1,15 +1,19 @@
+#import libraries
 import cv2
 import numpy as np
 
 img= np.zeros((512, 512, 3), np.uint8)
 
-print(img)
+print(img)  #fresh image
 
-cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 2)
-cv2.rectangle(img, (350, 100), (450, 200), (0, 0, 255), cv2.FILLED)
-cv2.circle(img, (150, 400), 50, (255, 0, 0), 3)
-cv2.putText(img, 'Shapes', (75, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 1)
+cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 2)  #draw line
 
-cv2.imshow('Output', img)
+cv2.rectangle(img, (350, 100), (450, 200), (0, 0, 255), cv2.FILLED)  #rectangle
+
+cv2.circle(img, (150, 400), 50, (255, 0, 0), 3)  #circle
+
+cv2.putText(img, 'Shapes', (75, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 1)  #text
+
+cv2.imshow('Output', img)  #output
 
 cv2.waitKey(0)
